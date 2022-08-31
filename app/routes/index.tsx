9f81312a164d92942d71310056d11a24c2840d88
Håkon Krogh <hakonhk@gmail.com>
2022-08-31 13:30:55 +0200

    Verify jwt

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -7,32 +7,57 @@ export const loader: LoaderFunction = async (params) => {
   const url = new URL(params.request.url);
   const signature = url.searchParams.get("crystallizeSignature");
 
-  let valid = false;
   if (!signature) {
-    return json({ valid });
+    return json({
+      message: "Missing `crystallizeSignature` in URL search params",
+    });
   }
 
+  let jwtSecret: string | null =
+    (params.context?.CRYSTALLIZE_JWT_SECRET as string) || null;
+
+  /**
+   * For DEMO purposes, pull secret from a search param. This
+   * should NEVER be done in a production-like app
+   */
+  if (!jwtSecret) {
+    jwtSecret = url.searchParams.get("secret");
+  }
+
+  if (!jwtSecret) {
+    return json({
+      jwtSecret,
+      message:
+        "No JWT secret provided. You can pass it as a query param for this debug app: ?secret=...",
+    });
+  }
+
+  const jwtVerified = await jwt.verify(signature, jwtSecret);
   const jwtDecoded = await jwt.decode(signature);
 
-  // Get the tenant in the context
-  // const { tenant } = decoded.payload;
-  // if (!tenant) {
-  //   return json({ valid });
-  // }
-
-  // Validate by pulling the tenant secret
-
-  return json({ valid, jwt: jwtDecoded });
+  return json({ jwtVerified, jwtSecret, jwt: jwtDecoded });
 };
 
 export default function Index() {
   const data = useLoaderData();
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div
+      style={{
+        fontFamily: "system-ui, sans-serif",
+        lineHeight: "1.4",
+        padding: 20,
+      }}
+    >
       <h1>Crystallize Debug App</h1>
 
-      <h2>Crystallize signature</h2>
+      <h2>
+        {data.jwtVerified
+          ? "Hooray! I trust that Crystallize has initiated the request to open me. ⭐️"
+          : "Hm. I do not trust you. Suspecting ill intent. 🏴‍☠️"}
+      </h2>
+
+      <h3 style={{ fontSize: "1rem" }}>Context</h3>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
